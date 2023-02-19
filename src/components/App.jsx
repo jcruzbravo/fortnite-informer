14719d312222769e5695a08118ef72d36d017c46
jcruzbravo <L18390012@chetumal.tecnm.mx>
2023-02-18 19:13:53 -0500

    Add component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,14 @@
 import React from "react";
 import '../styles/global.scss';
+import Header from "@components/Header";
 
 const App = () => {
-  return <h1>Fortnite Informer</h1>;
+  return (
+    <>
+      <Header/>
+      <h1>Fortnite Informer</h1>
+    </>
+  );
 };
 
 export default App;
