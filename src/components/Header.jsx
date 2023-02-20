a6fd6f71a83b28205f4afa00997843becd702691
jcruzbravo <L18390012@chetumal.tecnm.mx>
2023-02-20 12:07:33 -0500

    Add options to browse

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,19 @@
 import React from "react";
 import '@styles/Header.scss';
-import logo from '@logo/logo.png';
+import logo from '@assets/logo.png';
 
 const Header = () => {
   return (
     <nav>
       <div className="navbar">
         <img src={logo} alt="Logo Fortnite" />
+        <ul>
+          <li><a href="">Shop</a></li>
+          <li><a href="">Cosmetics</a></li>
+          <li><a href="">Map</a></li>
+          <li><a href="">Creator code</a></li>
+          {/* <li>Search</li> */}
+        </ul>
       </div>
     </nav>
   );
