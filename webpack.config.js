7c8cfc70c93a22a1bb2158ddea7404f90c71623b
jcruzbravo <L18390012@chetumal.tecnm.mx>
2023-02-20 10:09:47 -0500

    Change alias

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,7 @@ module.exports = {
     alias: {
       '@components': path.resolve(__dirname, 'src/components/'),
       '@styles': path.resolve(__dirname, 'src/styles/'),
-      '@logo': path.resolve(__dirname, 'src/assets/')
+      '@assets': path.resolve(__dirname, 'src/assets/')
 
     }
   },
