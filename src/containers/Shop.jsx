6fc4d5060af8fd125d51abe2f9060626c97622eb
jcruzbravo <L18390012@chetumal.tecnm.mx>
2023-02-22 21:15:01 -0500

    Add components for each category

--- src/containers/Shop.jsx
+++ src/containers/Shop.jsx
@@ -1,28 +1,19 @@
-import React from 'react';
-import ItemShop from '@components/ItemShop';
-import Header from '@components/Header'
-import useGetItemsShop from '@hooks/useGetItemsShop';
-import '@styles/Shop.scss';
-
-const API_URL = 'https://fortnite-api.com/v2/shop/br/combined';
+import React from "react";
+import Header from "@components/Header";
+import FeaturedShop from "@components/FeaturedShop";
+import DailyShop from "@components/DailyShop";
+import "@styles/Shop.scss";
 
 const Shop = () => {
-  const items = useGetItemsShop(API_URL);
-  const itemsWithBundle = items.filter(item => item.bundle != null)
-  console.log(itemsWithBundle.length)
   return (
     <>
-    <Header/>
-    <section className='items-shop-container'>
-      <div className='Shop'>
-        {itemsWithBundle.map((item) => (
-          <ItemShop key={`is-${item.bundle.name}`} item={item} vBuckIcon={item}/>
-        ))}
+      <Header />
+      <div className="Shop">
+        <FeaturedShop />
+        <DailyShop />
       </div>
-    </section>
     </>
-
   );
-}
+};
 
 export default Shop;
