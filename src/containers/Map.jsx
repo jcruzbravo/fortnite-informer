9135e00cac7096ac7f4db77f203f45bc897ded93
jcruzbravo <L18390012@chetumal.tecnm.mx>
2023-02-25 18:50:03 -0500

    Add static map url and styles

--- src/containers/Map.jsx
+++ src/containers/Map.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import Header from '@components/Header';
+import '@styles/Map.scss';
 
 const Map = () => {
   return (
     <>
       <Header/>
       <div className='Map'>
-        <img src="https://fortnite-api.com/images/map_en.png" alt="" style={{width: 800}}/>
+        <div className='map-container'>
+          <img src="https://fortnite-api.com/images/map_en.png" alt=""/>
+        </div>
       </div>
     </>
   )
