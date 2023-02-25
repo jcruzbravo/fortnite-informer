8f987d131f1e4f6e507076785c5ec963a9d1b8d6
jcruzbravo <L18390012@chetumal.tecnm.mx>
2023-02-25 18:38:33 -0500

    Add useEffect to load a component after some time

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Header from "@components/Header";
 import NewsBrList from "@containers/NewsBrList";
 import NewsSwList from "@containers/NewsSwList";
 import "@styles/Home.scss";
 
 const Home = () => {
+  const [loaded, setLoaded] = useState(false)
+  useEffect(() => {
+    setTimeout(() => {
+      setLoaded(true)
+    }, 5000);
+  }, [])
+  
   return (
     <>
       <Header />
       <div className="Home">
-        <NewsBrList />
-        <NewsSwList />
+          <NewsBrList />
+        {loaded && (
+          <NewsSwList />
+        )}
       </div>
     </>
   );
