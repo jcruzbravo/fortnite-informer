08b4b5f7ef6a11ec90b6466f9186af1f4ced8815
jcruzbravo <L18390012@chetumal.tecnm.mx>
2023-02-24 20:27:04 -0500

    Add skeleton loader to store featured

--- src/components/FeaturedShop/FeaturedShop.jsx
+++ src/components/FeaturedShop/FeaturedShop.jsx
@@ -1,27 +1,52 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ItemFeaturedShop from "./ItemFeaturedShop";
 import useGetItemsFeaturedShop from "@hooks/useGetItemsFeaturedShop";
-import '@styles/FeaturedShop.scss'
+import "@styles/FeaturedShop.scss";
+import LoaderFeatured from "./LoaderFeatured";
 
 const API_URL = "https://fortnite-api.com/v2/shop/br/combined";
 
 const FeaturedShop = () => {
   const items = useGetItemsFeaturedShop(API_URL);
   const itemsWithBundle = items.filter((item) => item.bundle != null);
-  return (
-    <section className="items-featured-shop-container">
-      <h2>Featured Bundle</h2>
-      <div className="FeaturedShop">
-        {itemsWithBundle.map((item) => (
-          <ItemFeaturedShop
-            key={`is-${item.bundle.name}`}
-            item={item}
-            background={item.newDisplayAsset.materialInstances[0].images.Background}
-          />
-        ))}
-      </div>
-    </section>
-  );
+
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 5000);
+  }, []);
+
+  if (loading) {
+    return (
+      <section className="items-featured-shop-container">
+        <h2>Featured Bundle</h2>
+        <div className="FeaturedShop">
+          {itemsWithBundle.map((item) => (
+            <LoaderFeatured key={`is-${item.bundle.name}`} item={item} />
+          ))}
+        </div>
+      </section>
+    );
+  } else {
+    return (
+      <section className="items-featured-shop-container">
+        <h2>Featured Bundle</h2>
+        <div className="FeaturedShop">
+          {itemsWithBundle.map((item) => (
+            <ItemFeaturedShop
+              key={`is-${item.bundle.name}`}
+              item={item}
+              background={
+                item.newDisplayAsset.materialInstances[0].images.Background
+              }
+            />
+          ))}
+        </div>
+      </section>
+    );
+  }
 };
 
 export default FeaturedShop;
---
+++ src/components/FeaturedShop/LoaderFeatured.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
+
+const LoaderFeatured = () => {
+  return (
+    <div className="ItemFeaturedShop">
+      <div className="item-featured-shop-info">
+        <div className="item-featured-shop-img">
+          <Skeleton
+            count={1}
+            height={340}
+            width={340}
+            baseColor={"white"}
+            highlightColor={"#e3e3e3"}
+          />
+        </div>
+      </div>
+      <div className="item-featured-shop-info">
+        <p style={{ width: 320, textAlign: "center" }}>
+          <Skeleton
+            count={2}
+            height={20}
+            width={300}
+            baseColor={"white"}
+            highlightColor={"#e3e3e3"}
+          />
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default LoaderFeatured;
