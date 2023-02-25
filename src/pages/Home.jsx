0ebbc0acffc16a75d0f7ecce42207953fa46841c
jcruzbravo <L18390012@chetumal.tecnm.mx>
2023-02-25 18:59:41 -0500

    Remove NewsSw component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,25 +1,15 @@
 import React, { useState, useEffect } from "react";
 import Header from "@components/Header";
 import NewsBrList from "@containers/NewsBrList";
-import NewsSwList from "@containers/NewsSwList";
 import "@styles/Home.scss";
 
 const Home = () => {
-  const [loaded, setLoaded] = useState(false)
-  useEffect(() => {
-    setTimeout(() => {
-      setLoaded(true)
-    }, 5000);
-  }, [])
   
   return (
     <>
       <Header />
       <div className="Home">
-          <NewsBrList />
-        {loaded && (
-          <NewsSwList />
-        )}
+        <NewsBrList />
       </div>
     </>
   );
