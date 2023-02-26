1a1917fe9b9f02e2cbfb784071d28fedf2b19894
jcruzbravo <L18390012@chetumal.tecnm.mx>
2023-02-25 19:37:03 -0500

    Change anchor to Link

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,16 @@
 import React from "react";
 import '@styles/Header.scss';
 import logo from '@assets/logo.png';
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <nav>
       <div className="navbar">
-        <a href="/"><img src={logo} alt="Logo Fortnite" /></a>
+        <Link><img src={logo} alt="Logo Fortnite" /></Link>
         <ul>
-          <li><a href="./shop">Shop</a></li>
-          {/* <li><a href="">Cosmetics</a></li> */}
-          <li><a href="./map">Map</a></li>
-          <li><a href="">Creator code</a></li>
-          {/* <li>Search</li> */}
+          <li><Link to="/shop"></Link></li>
+          <li><Link to="/map"></Link></li>
         </ul>
       </div>
     </nav>
