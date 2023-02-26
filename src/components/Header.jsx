c4c6ba362dcd3a47c12cd054e12b404d7bdc97f6
jcruzbravo <L18390012@chetumal.tecnm.mx>
2023-02-25 19:50:41 -0500

    Change routes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,22 @@
 import React from "react";
-import '@styles/Header.scss';
-import logo from '@assets/logo.png';
+import "@styles/Header.scss";
+import logo from "@assets/logo.png";
 import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <nav>
       <div className="navbar">
-        <Link to="#/"><img src={logo} alt="Logo Fortnite" /></Link>
+        <Link>
+          <img src={logo} alt="Logo Fortnite" />
+        </Link>
         <ul>
-          <li><Link to="#/shop"></Link></li>
-          <li><Link to="#/map"></Link></li>
+          <li>
+            <Link to="/shop"></Link>
+          </li>
+          <li>
+            <Link to="/map"></Link>
+          </li>
         </ul>
       </div>
     </nav>
