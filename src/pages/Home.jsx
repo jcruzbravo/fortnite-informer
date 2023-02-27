f49f3a6fe11447ece3c83ca4587587220d9a3729
jcruzbravo <L18390012@chetumal.tecnm.mx>
2023-02-27 14:04:01 -0500

    Test 2

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,12 @@
 import React from "react";
 import Header from "@components/Header";
-import NewsBrList from "@containers/NewsBrList";
+//import NewsBrList from "@containers/NewsBrList";
 import "@styles/Home.scss";
 
 const Home = () => {
   
   return (
-    <>
-      <Header />
-      {/* <div className="Home">
-        <NewsBrList />
-      </div> */}
-    </>
+    <Header/>
   );
 };
 
