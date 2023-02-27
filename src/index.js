225ce73949ab1b8ff590d37bbdd6d8ad526783c4
jcruzbravo <L18390012@chetumal.tecnm.mx>
2023-02-27 13:22:30 -0500

    Delete BrowserRouter

--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "@routes/App";
-import { BrowserRouter } from "react-router-dom";
 
 const root = createRoot(document.getElementById("app"));
 root.render(
-  <BrowserRouter>
     <App />
-  </BrowserRouter>
 );
