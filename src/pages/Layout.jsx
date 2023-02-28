84e77ac8164592a231faefc52141023df7a29d0a
jcruzbravo <L18390012@chetumal.tecnm.mx>
2023-02-28 13:54:49 -0500

    Add home to prefix

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -11,7 +11,7 @@ const Layout = () => {
           <img src={logo} alt="Logo Fortnite" />
           <ul>
             <li>
-              <Link to="/">Home</Link>
+              <Link to="/home">Home</Link>
             </li>
             <li>
               <Link to="/news">News</Link>
