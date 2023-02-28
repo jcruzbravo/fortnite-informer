795ff26917670cbf3de443b9c424452a59a561ca
jcruzbravo <L18390012@chetumal.tecnm.mx>
2023-02-28 13:21:04 -0500

    Remove Header component

--- src/containers/Map.jsx
+++ src/containers/Map.jsx
@@ -1,18 +1,14 @@
-import React from 'react'
-import Header from '@components/Header';
-import '@styles/Map.scss';
+import React from "react";
+import "@styles/Map.scss";
 
 const Map = () => {
   return (
-    <>
-      <Header/>
-      <div className='Map'>
-        <div className='map-container'>
-          <img src="https://fortnite-api.com/images/map_en.png" alt=""/>
-        </div>
+    <div className="Map">
+      <div className="map-container">
+        <img src="https://fortnite-api.com/images/map_en.png" alt="" />
       </div>
-    </>
-  )
-}
+    </div>
+  );
+};
 
 export default Map;
--- src/containers/Shop.jsx
+++ src/containers/Shop.jsx
@@ -1,5 +1,4 @@
 import React, { useRef } from "react";
-import Header from "@components/Header";
 import FeaturedShop from "@components/FeaturedShop/FeaturedShop";
 import DailyShop from "@components/DailyShop/DailyShop";
 import "@styles/Shop.scss";
@@ -11,25 +10,22 @@ const Shop = () => {
   const loaded = useHandleScroll(divFeaturedShop);
 
   return (
-    <>
-      <Header />
-      <div className="Shop">
-        <div ref={divFeaturedShop} className="featured">
-          <FeaturedShop />
-        </div>
-        {loaded && (
-          <div className="daily">
-            <DailyShop />
-          </div>
-        )}
-
-        {loaded && (
-          <div className="without">
-            <WithoutBundle />
-          </div>
-        )}
+    <div className="Shop">
+      <div ref={divFeaturedShop} className="featured">
+        <FeaturedShop />
       </div>
-    </>
+      {loaded && (
+        <div className="daily">
+          <DailyShop />
+        </div>
+      )}
+
+      {loaded && (
+        <div className="without">
+          <WithoutBundle />
+        </div>
+      )}
+    </div>
   );
 };
 
