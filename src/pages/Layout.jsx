8df5931ccd4719f3e889ef495510b8af84dcfabc
jcruzbravo <L18390012@chetumal.tecnm.mx>
2023-02-28 13:21:13 -0500

    Add Layout component

---
+++ src/pages/Layout.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import "@styles/Header.scss";
+import logo from "@assets/logo.png";
+import { Link, Outlet } from "react-router-dom";
+
+const Layout = () => {
+  return (
+    <>
+      <nav>
+        <div className="navbar">
+          <Link to="/">
+            <img src={logo} alt="Logo Fortnite" />
+          </Link>
+          <ul>
+          <li>
+              <Link to="/news">News</Link>
+            </li>
+            <li>
+              <Link to="/shop">Shop</Link>
+            </li>
+            <li>
+              <Link to="/map">Map</Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+      <Outlet />
+    </>
+  );
+};
+
+export default Layout;
