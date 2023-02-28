31666f50aa36832b934cb3e7aa1994480fa30dfd
jcruzbravo <L18390012@chetumal.tecnm.mx>
2023-02-28 13:21:19 -0500

    Add Home component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,16 @@
 import React from "react";
-import Header from "@components/Header";
-import NewsBrList from "@containers/NewsBrList";
 import "@styles/Home.scss";
 
 const Home = () => {
-  
   return (
-    <>
-      <Header />
-      <div className="Home">
-        <NewsBrList />
-      </div>
-    </>
+    <div className="Home">
+      <section className="section-home-container">
+        <div className="section-home-content">
+          <h1>What's Fortnite?</h1>
+          <p>Fortnite is a world full of experiences. Enter the island and compete to be the last player (or team) standing. Create an island with your own rules. Spend time with your friends on an island created by someone else. Or save the world by wiping out hordes of monsters with other players.</p>
+        </div>
+      </section>
+    </div>
   );
 };
 
