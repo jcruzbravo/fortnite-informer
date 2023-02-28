d1ad1bc8251c0a6b55e21113c16249fad24f7595
jcruzbravo <L18390012@chetumal.tecnm.mx>
2023-02-28 13:21:57 -0500

    Add StrictMode and BrowserRouter

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
-import React from "react";
+import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "@routes/App";
+import { BrowserRouter } from "react-router-dom";
 
 const root = createRoot(document.getElementById("app"));
 root.render(
-    <App />
+  <StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StrictMode>
 );
