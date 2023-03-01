8ab5a6bd3ac15107d734f3fd7256cc57ff09cf9d
jcruzbravo <L18390012@chetumal.tecnm.mx>
2023-03-01 13:59:43 -0500

    Add Hero to Home component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,10 +4,16 @@ import "@styles/Home.scss";
 const Home = () => {
   return (
     <div className="Home">
-      <section className="section-home-container">
-        <div className="section-home-content">
-          <h1>What's Fortnite?</h1>
-          <p>Fortnite is a world full of experiences. Enter the island and compete to be the last player (or team) standing. Create an island with your own rules. Spend time with your friends on an island created by someone else. Or save the world by wiping out hordes of monsters with other players.</p>
+      <section className="hero">
+        <div className="hero-content">
+          <h2 className="hero-title">What's Fortnite?</h2>
+          <p className="hero-description">
+            Fortnite is a world full of experiences. Enter the island and
+            compete to be the last player (or team) standing. Create an island
+            with your own rules. Spend time with your friends on an island
+            created by someone else. Or save the world by wiping out hordes of
+            monsters with other players.
+          </p>
         </div>
       </section>
     </div>
